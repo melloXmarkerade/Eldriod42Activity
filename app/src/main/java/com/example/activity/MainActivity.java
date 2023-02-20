ee7554fa433caa856924684e348e10470fc14404
markerade2 <markerade234@gmail.com>
2023-02-20 13:07:37 +0800

    Button Functionality Registration Page to Login Page Vice Versa

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Login, Registration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Login = findViewById(R.id.Login_But);
+        Registration = findViewById(R.id.RegisterPage);
+
+        Registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegisterPage.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/activity/RegisterPage.java
+++ app/src/main/java/com/example/activity/RegisterPage.java
@@ -2,13 +2,26 @@ package com.example.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterPage extends AppCompatActivity {
 
+    Button RecordBut, LoginPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        LoginPage = findViewById(R.id.LoginPage);
+
+        LoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterPage.this,MainActivity.class));
+            }
+        });
     }
 }
